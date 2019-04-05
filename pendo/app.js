f7e54cef05f6897158ba9106775786abe79877f4
David Burgess <i.am@dburg.es>
2019-04-05 11:27:21 -0400

    Update app.js

--- pendo/app.js
+++ pendo/app.js
@@ -1,28 +1,166 @@
-function playPad(keyCode) {
-  const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
-  const pad = document.querySelector(`.drum-pads>div[data-key="${keyCode}"]`);
-  if(!audio) return;
-  audio.currentTime = 0;
-  audio.play();
-  pad.classList.add('playing');
+let $ = document.querySelector.bind(document);
+
+let chromaticOrb = $('#chromatic-orb');
+
+window.requestAnimationFrame(
+  function update(){
+    chromaticOrb.style.height = chromaticOrb.getBoundingClientRect().width+'px';
+    let r = randomVal(155,255);
+    let g = randomVal(50,255);
+    let b = randomVal(50,255);
+    let stopCondition = true; //!$('button:active')
+    if(stopCondition){
+      chromaticOrb.style.backgroundColor = `rgb(${r},${g},${b})`;
+      chromaticOrb.innerText = (Number(chromaticOrb.innerText)+1) % 9999
+    } 
+    window.requestAnimationFrame(update);
+  }
+)
+
+function randomVal(min, max) {
+  return Math.floor(Math.random() * (max - min) + 1) + min;
 }
 
-function keydownHandiler(e) {
-  playPad(e.keyCode);
+Array.from(document.querySelectorAll('.stopper')).forEach(function(stopper){
+  bindOrb(stopper,'mousedown') 
+  bindOrb(stopper,'click');
+  bindOrb(stopper,'mouseup');
+})
+function bindOrb(stopper, event){
+  stopper.addEventListener(event, function(){
+    let orb = document.querySelector(`#${event}-orb`);
+    orb.style.backgroundColor = chromaticOrb.style.backgroundColor;
+    orb.innerText = chromaticOrb.innerText;
+    // orb.style.transform = getComputedStyle(chromaticOrb)['transform'];
+  });
 };
 
-function removeTransitionClass(e) {
-  if(e.propertyName !== 'transform') return;
-  this.classList.remove('playing');
-}
+let clickStart = 0;
+let clickDifference = 0;
+Array.from(document.querySelectorAll('.stopper')).forEach(function(stopper){
+  
+  stopper.addEventListener(
+    'click',
+    ()=>{
+      if(!isFeatureClickPropertiesEnabled) clickStart = performance.now();
+      document.querySelector('#click-start').innerHTML = clickStart.toFixed(5)+' ms';
+    },
+    true
+  )
+  stopper.addEventListener(
+    'click',
+    ()=>{
+      clickEnd = performance.now();
+      document.querySelector('#click-end').innerHTML = clickEnd.toFixed(5)+' ms';
+      let difference = (clickEnd - clickStart).toFixed(5);
+      if(difference > 1){
+        difference = '<span style="color:red">' + difference +' ms' + '</span>';
+      }else{
+        difference += ' ms';
+      }
+      document.querySelector('#click-difference').innerHTML = difference;
+    },
+    false
+  )
+});
 
-function padClickHandiler(e) {
-  const target = e.target.tagName === "SPAN" ? e.target.parentNode : e.target;
-  playPad(target.attributes['data-key'].value);
-}
+let isFeatureClickPropertiesEnabled = false;
+let documentClickListener = undefined;
 
-const pads = document.querySelectorAll('.drum-pads>div');
-pads.forEach(pad => { pad.addEventListener('transitionend', removeTransitionClass);
-pad.addEventListener('click', padClickHandiler)
-                    });
-window.addEventListener('keydown', keydownHandiler);
+$('#rule-activation input').addEventListener('change',(evt)=>{
+  isFeatureClickPropertiesEnabled = evt.target.checked;
+  if(isFeatureClickPropertiesEnabled){
+    documentClickListener = document.addEventListener('click',function(clickEvent){
+      clickStart = performance.now();
+      // putting in dummy matchers to test performance difference
+      ['#STaaAHP', "#NOPaaE", "#NOaaPE",'#STAaaHP', "#NOPE", "#NOPE", rule].forEach(rule=>{
+        if(rule && clickEvent.target.matches(rule)){
+          let evalResult;
+          try{
+            evalResult = eval(collector);
+          }catch(err){
+            console.error('collector function failed')
+          }
+
+          $('#new-property').innerText = evalResult
+            ? `"${propertyName} : "${evalResult}"`
+            : ``;
+          
+          setTimeout(()=>{
+            let lastEvent = getLastClickEvent();
+            $('#existing-track-event').title = JSON.stringify(lastEvent);
+            lastEvent.eventProperties = lastEvent.eventProperties || {};
+            lastEvent.eventProperties[propertyName]  = evalResult;
+          },0)
+        }
+      })  
+    }, true);
+  }else{
+    documentClickListener && documentClickListener();
+  }
+});
+
+let rule = '';
+$('#rule').addEventListener('change',(evt)=>{
+  rule = evt.target.value;
+});
+
+let propertyName = '';
+$('#property-name').addEventListener('change',(evt)=>{
+  propertyName = evt.target.value;
+});
+
+let collector = '';
+let collectorRunner = debounce((evt)=>{
+  collector = evt.target.value;
+  let result = "";
+  let startTime = performance.now();
+  try{
+    result = eval(collector) 
+  }catch(err){
+    result = `<span style="color:red">${err.message}</span>`;
+  }
+  let endTime = performance.now();
+  let executionTime = (endTime - startTime).toFixed(5);
+  $('#collector-time').innerHTML = executionTime+' ms';
+  if(executionTime>50){
+    $('#collector-time').style.color="red";
+  }else{
+    $('#collector-time').style.color="black";
+  }
+  $('#collector-result').innerHTML = '<strong>'+result+'</strong';
+},500);
+$('#collector').addEventListener('keydown',collectorRunner);
+$('#collector').addEventListener('focus',collectorRunner);
+
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+function getLastClickEvent(){
+  if(!pendo || pendo.isDebuggingEnabled() === "No"){
+    return "run pendo.enableDebugging() to view click props"
+  }
+  let index = 1
+  let type;
+  let cacheLength = pendo.eventCache.length;
+  
+  while(type !== 'click' && index < cacheLength){
+    event = pendo.eventCache[cacheLength-index];
+    type = event.type;
+    index++;
+  }
+
+  return event;
+}
